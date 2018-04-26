b5687c8ba2fea9814126c048109d2607130deaee
Kavi Jivan <kjivan@gmail.com>
2018-04-25 20:36:15 -0400

    Adds fasd fish plugin

--- dev-env/install-packages.fish
+++ dev-env/install-packages.fish
@@ -17,30 +17,38 @@ python3 \
 node \
 heroku
 
+# fish fasd plugin
+if type -q fresco
+else
+    curl https://raw.githubusercontent.com/masa0x80/fresco/master/install | fish
+    exec fish -l
+    fresco fishgretel/fasd
+end
+
 # plug for neovim
 if test ! -e ~/.local/share/nvim/site/autoload/plug.vim
-  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 end
 
 # wemux
 if test ! (grep $USER /usr/local/etc/wemux.conf)
-  sed -i '' "s/brew/$USER/" /usr/local/etc/wemux.conf
+    sed -i '' "s/brew/$USER/" /usr/local/etc/wemux.conf
 end
 
 # fzf
 if test ! (grep fzf_key_bindings $HOME/.config/fish/functions/fish_user_key_bindings.fish)
-  yes | sh (brew --prefix)/opt/fzf/install
+    yes | sh (brew --prefix)/opt/fzf/install
 end
 
 # watchman
 if test ! (grep kern.maxfiles=10485760 /etc/sysctl.conf)
-  sudo sysctl -w kern.maxfiles=10485760
-  sudo fish -c 'echo kern.maxfiles=10485760 >> /etc/sysctl.conf'
+    sudo sysctl -w kern.maxfiles=10485760
+    sudo fish -c 'echo kern.maxfiles=10485760 >> /etc/sysctl.conf'
 end
 if test ! (grep kern.maxfilesperproc=1048576 /etc/sysctl.conf)
-  sudo sysctl -w kern.maxfilesperproc=1048576
-  sudo fish -c 'echo kern.maxfilesperproc=1048576 >> /etc/sysctl.conf'
+    sudo sysctl -w kern.maxfilesperproc=1048576
+    sudo fish -c 'echo kern.maxfilesperproc=1048576 >> /etc/sysctl.conf'
 end
 
 brew cask install \
