08fa4d0463d7c70726d9646cc6601c60c4064401
Kavi Jivan <kavi.jivan@viavisolutions.com>
2018-12-13 23:52:16 -0500

    Clean up

--- .config/fish/functions/cppwatch.fish
+++ .config/fish/functions/cppwatch.fish
@@ -1,3 +1,9 @@
 function cppwatch
-    watchman-make -p '**/*.cpp' '**/*.h' '**/*.c' '**/*.inl' '**/CMakeLists.txt' --run "$argv"
+    watchman-make -p \
+        '**/*.cpp' \
+        '**/*.h' \
+        '**/*.c' \
+        '**/*.inl' \
+        '**/CMakeLists.txt' \
+        --run "$argv"
 end
