750b99e618f27d0cd596406ffee2fc4e4cc54b7c
Kavi Jivan <kavi.jivan@viavisolutions.com>
2019-05-28 13:43:50 -0400

    Changes references of nvim to rely on alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,21 @@
 #!/usr/bin/env fish
+
+if test -d /home/linuxbrew/.linuxbrew/bin
+    set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
+end
+if test -d /home/linuxbrew/.linuxbrew/sbin
+    set -gx PATH /home/linuxbrew/.linuxbrew/sbin $PATH
+end
+if test -d /usr/local/go/bin
+    set -gx PATH $PATH /usr/local/go/bin
+    set -gx GOPATH $HOME/go
+end
+if test -d $HOME/.cargo/bin
+    set -gx PATH $PATH $HOME/.cargo/bin
+end
+
 set -xg fish_greeting ""
+
 set -xg VISUAL nvim
 set -xg EDITOR $VISUAL
 set -xg GIT_EDITOR $VISUAL
@@ -14,6 +30,16 @@ function grep
     command grep --color=auto $argv
 end
 
+alias c='cd (fd -t d | fzf)'
+alias ch='cd (fd --search-path $HOME -t d | fzf)'
+alias v='vi (fd | fzf)'
+alias vh='vi (fd --search-path $HOME | fzf)'
+
+if type -q nvim
+    alias vim=nvim
+    alias vi=nvim
+end
+
 if type -q exa
     alias l='exa'
     alias ls='exa'
@@ -22,41 +48,6 @@ else
     alias l='ls --color'
     alias ls='ls --color'
     alias ll='ls --color -l'
-end
-
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-
-alias md='mkdir -p'
-alias rd=rmdir
-
-# neovim
-if type -q nvim
-    alias vim=nvim
-    alias vi=nvim
-end
-
-alias g='git'
-
-alias c='cd (fd -t d | fzf)'
-alias ch='cd (fd --search-path $HOME -t d | fzf)'
-alias v='nvim (fd | fzf)'
-alias vh='nvim (fd --search-path $HOME | fzf)'
-
-if test -d /home/linuxbrew/.linuxbrew/bin
-    set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
-end
-if test -d /home/linuxbrew/.linuxbrew/sbin
-    set -gx PATH /home/linuxbrew/.linuxbrew/sbin $PATH
-end
-if test -d /usr/local/go/bin
-    set -gx PATH $PATH /usr/local/go/bin
-    set -gx GOPATH $HOME/go
-end
-if test -d $HOME/.cargo/bin
-    set -gx PATH $PATH $HOME/.cargo/bin
 end
 
 [ -f /home/linuxbrew/.linuxbrew/share/autojump/autojump.fish ]; and source /home/linuxbrew/.linuxbrew/share/autojump/autojump.fish
