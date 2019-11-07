0a67f58c03c4b8897d5f06e396b53ed7110db326
Kavi Jivan <kavi.jivan@nextcentury.com>
2019-11-07 15:57:36 -0500

    Add username and hostname to prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,3 @@
-function fish_prompt
-    set_color normal; echo -n (prompt_pwd)
-    set_color brgreen; echo -n '> '
+function fish_prompt --description 'Write out the prompt'
+    printf '%s%s@%s%s %s%s%s> ' (set_color yellow) (whoami) (set_color purple) (prompt_hostname) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
-
