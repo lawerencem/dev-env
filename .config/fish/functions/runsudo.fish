38f19ade3648788d5c4f431a08050202891357c7
Kavi Jivan <kjivan@gmail.com>
2020-08-11 14:50:17 -0400

    runsudo does not run automatically

--- .config/fish/functions/runsudo.fish
+++ .config/fish/functions/runsudo.fish
@@ -1,5 +1,6 @@
 function runsudo --description 'Run current command line as root'
+    set cursor_pos (echo (commandline -C) + 5 | bc)
     commandline -C 0
     commandline -i 'sudo '
-    commandline -f execute
+    commandline -C "$cursor_pos"
 end
