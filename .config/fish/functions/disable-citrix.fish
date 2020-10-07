026a65f1b3e9effbdb480c62a9d8001b66f99969
Kavi Jivan <kjivan@gmail.com>
2020-10-07 08:59:56 -0400

    Adds fish function to disable citrix startup

---
+++ .config/fish/functions/disable-citrix.fish
@@ -0,0 +1,7 @@
+function disable-citrix
+
+  sudo find /Library/LaunchAgents -type f -name 'com.citrix*' -exec sh -c 'x="{}"; mv "$x" "${x}_bak"' \;
+  sudo find /Library/LaunchDaemons -type f -name 'com.citrix*' -exec sh -c 'x="{}"; mv "$x" "${x}_bak"' \;
+  echo "Success: Citrix Workspace Disabled from Launching at Startup"
+
+end
